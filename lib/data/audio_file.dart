50b1a1939150ea41846179490c5306bbd2bdd3c3
Gaurav Katva <gaurav@grappus.com>
2023-04-25 14:34:00 +0530

    first: initial commit

---
+++ lib/data/audio_file.dart
@@ -0,0 +1,11 @@
+class AudioFile {
+  String title;
+  String thumbnailUrl;
+  String url;
+
+  AudioFile({
+    required this.title,
+    required this.thumbnailUrl,
+    required this.url,
+  });
+}
